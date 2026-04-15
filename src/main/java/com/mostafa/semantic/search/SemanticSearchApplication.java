bf26c9b241bfd9089a6eeb71e7fcdea1c90b9d8a
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-15 23:23:49 +0200

    initialize project with Maven setup and Spring Boot application

---
+++ src/main/java/com/mostafa/semantic/search/SemanticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa.semantic.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mostafa/semantic/search/SemanticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.semantic.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
